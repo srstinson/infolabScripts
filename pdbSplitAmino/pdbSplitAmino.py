a985a9c8158d8e5a17aff6e62e07469eecda6dae
Steven Stinson <srs514@corona1.cc.lehigh.edu>
2013-04-23 12:49:16 -0400

    1. Changed pdbLines to a queue for better performance.\n2. Made getAcid yield instead of building a list, changed other methods to reflect type change (i.e. list->generator)\n3. Added usage() function that displays when incorrectly used.

--- pdbSplitAmino/pdbSplitAmino.py
+++ pdbSplitAmino/pdbSplitAmino.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from collections import deque
 
 ####################################################
 # Author: Steven Stinson
@@ -13,37 +14,51 @@ AMINO_ACID_END = 20
 
 def getAcid(pdbLines):
     acid = ""
-    acidLines = []
     while(len(pdbLines)>0):
-        line = pdbLines.pop(0).strip()
+        line = pdbLines.popleft().strip()
         if (line.startswith("ATOM ")):
             currentAcid = line[AMINO_ACID_BEGIN:AMINO_ACID_END]
             if (acid==""):
                 acid = currentAcid
             elif(currentAcid!=acid):
-                return acidLines
-            else:
-                print line
-                acidLines.append(line+"\n")
-    return []
+		pdbLines.appendleft(line)
+                break
+            #print line
+            yield line+"\n"
 
 def outputAcidFile(dir,id,acidLines):
-    #print acidLines[0]
-    outputFileName = os.path.join(dir,id+acidLines[0][AMINO_ACID_BEGIN:AMINO_ACID_END]+acidLines[0][23:26].strip())
+    firstLine = acidLines.__iter__().next()
+    outputFileName = os.path.join(dir,id+firstLine[AMINO_ACID_BEGIN:AMINO_ACID_END]+firstLine[23:26].strip())
 
     outFile = open(outputFileName+".pdb","w")
+    outFile.write(firstLine)
     outFile.writelines(acidLines)
 
 def main():
+    if len(sys.argv) != 2:
+	usage()
+	exit()
+
     pdbFile = open(sys.argv[1],"r")
     splitFile = os.path.split(sys.argv[1])
     id = splitFile[1][0:4]
     dir = splitFile[0]
-    pdbLines = pdbFile.readlines()
+    pdbLines = deque(pdbFile.readlines())
+   
     while (len(pdbLines)>0):
         acidLines = getAcid(pdbLines)
-        if (len(acidLines)!=0):
-            outputAcidFile(dir,id,acidLines)
+        outputAcidFile(dir,id,acidLines)
+
+def usage():
+	print "USAGE:"
+	print "--------------------------------------------------"
+	print "python pdbSplitAmino.py XXXX.pdb"
+	print "Will generate n files, where n is the number of"
+	print "amino acids in the pdb file, named:"
+	print "XXXXYYYZZ.pdb"
+	print "Where XXXX is the pdb id, YYY is the acid name, and ZZ is the acid number"
+	print "For any questions please contact me at srs514@lehigh.edu"
+	print "--------------------------------------------------"
 
 if __name__ == "__main__":
             main()
